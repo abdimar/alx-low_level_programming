c25277e8284cd90b0ca3e81cbf8574c90b2c5adf
abdimar <abdallahomarabdallah92@gmail.com>
2022-10-19 06:49:28 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 /**
  *main-program entry point.
- *
+ i*
  *Return: 1 on success.
  */
 
@@ -10,7 +10,7 @@ int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", 59);
+		write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
 
