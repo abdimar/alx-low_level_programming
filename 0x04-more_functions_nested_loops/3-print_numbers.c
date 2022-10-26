63ea48f805b7df051a2b0539e5baa365860e9aee
abdimar <abdallahomarabdallah92@gmail.com>
2022-10-26 15:22:48 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *
+ * print_numbers - prints the numbers
+ *
+ * Return: void
+ *
+ */
+ *
+ *      void print_numbers(void)
+ *
+ *   {
+ *
+ *           char c;
+ *
+ *
+ *
+ *            for (c = '0'; i <= '9'; i++)
+ *
+ *       {
+ *
+ *                 _putchar(c);
+ *
+ *       }
+ *
+ *       _putchar('\n');
+ *
+ *    }  
+ *
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets the value of a vairiable 98
+ * @n : variable to be reset
+ * Return : void
+ */
+
+void reset_to_98(int*n)
+{
+	*n = 98;
+}
